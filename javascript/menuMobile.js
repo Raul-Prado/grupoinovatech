29649a13c104d764f1f8e7afdcb13c3ece077921
Raul-Prado <rauloliveiraprado@gmail.com>
2024-05-01 02:20:17 -0300

    pagina web com flebox

---
+++ javascript/menuMobile.js
@@ -0,0 +1,10 @@
+function clickMenu(){
+    var menu = document.querySelector('nav ul');
+    menu.classList.toggle('active'); // Adicionar ou remover a classe .active no menu
+    // let menu = document.getElementById("itens");
+    // if(menu.style.display == 'block'){
+    //     menu.style.display = 'none';
+    // }else{
+    //     menu.style.display = 'block';
+    // }
+}
